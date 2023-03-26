bdd8bdfdceb673d52ac2531e609fdfa55022ced7
August-Roosi <augustroosi@gmail.com>
2023-03-26 18:34:14 +0300

    Muudatused klassis, et CSVLuger klass saaks luua Ülekanne objekte.

--- src/main/java/oop/ryhmatoo/Ülekanne.java
+++ src/main/java/oop/ryhmatoo/Ülekanne.java
@@ -5,4 +5,21 @@ public class Ülekanne {
     private String teineOsapool;
     private Double summa;
     private Boolean onVäljaminek;
+
+    public Ülekanne(String kuupäev, String teineOsapool, Double summa, Boolean onVäljaminek) {
+        this.kuupäev = kuupäev;
+        this.teineOsapool = teineOsapool;
+        this.summa = summa;
+        this.onVäljaminek = onVäljaminek;
+    }
+
+    @Override
+    public String toString() {
+        return "Ülekanne{" +
+                "kuupäev='" + kuupäev + '\'' +
+                ", teineOsapool='" + teineOsapool + '\'' +
+                ", summa=" + summa +
+                ", onVäljaminek=" + onVäljaminek +
+                '}';
+    }
 }
