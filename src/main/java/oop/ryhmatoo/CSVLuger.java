ea30e335569668510bed9f26cd13c4ab9449c8ee
August-Roosi <augustroosi@gmail.com>
2023-03-26 18:32:26 +0300

    Tegin CSVLuger klass, mis loob Ülekanne objektid.

---
+++ src/main/java/oop/ryhmatoo/CSVLuger.java
@@ -0,0 +1,46 @@
+package oop.ryhmatoo;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CSVLuger {
+
+    public static List<Ülekanne> loeCSV(String failiNimi) {
+        ArrayList<Ülekanne> CSVSisu = new ArrayList<>();
+
+        try {
+            List<String> failiRead = Files.readAllLines(Paths.get(failiNimi), StandardCharsets.UTF_8);
+            failiRead = failiRead.stream()
+                    .map(s -> s.replaceAll("\"", ""))
+                    .collect(Collectors.toList());
+            for (String rida : failiRead) {
+                String[] sisu = rida.split(";");
+                switch (sisu[1]) {
+                    case "10", "82", "86", "Reatüüp":
+                        System.out.println(Arrays.toString(sisu));
+                        break;
+
+                    case "20":
+                        //Vaatab kas tegemist on kulutusega(K) või mittekulutusega(MK)
+                        boolean onVäljaminek = !sisu[9].equals("MK");
+                        //Eemaldab liigsed tühikud
+                        String teineOsapool = sisu[3].trim().replaceAll(" +", " ");
+                        //Muudab komakohaga arvu punktiga arvuks
+                        double summa = Double.parseDouble(sisu[5].replaceAll(",", "."));
+                        Ülekanne ülekanne = new Ülekanne(sisu[2], teineOsapool, summa, onVäljaminek);
+                        CSVSisu.add(ülekanne);
+                        break;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.format("I/O error: %s%n", ex);
+        }
+        return CSVSisu.stream().toList();
+    }
+}
