a4b4b50d66be2cddfadcc5c486e38537780c06ef
HMaandi <henrimaandi@gmail.com>
2023-03-27 19:46:39 +0300

    Algne server tehtud

--- server/src/main/java/oop/ryhmatoo/App.java
+++ server/src/main/java/oop/ryhmatoo/App.java
@@ -1,13 +1,25 @@
 package oop.ryhmatoo;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App
 {
-    public static void main( String[] args )
+    public static final int TYPE_UPLOAD = 1;
+    public static final int TYPE_GET_LEADERBOARD = 2;
+    public static final int TYPE_OK = 3;
+    public static final int TYPE_ERROR = 4;
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        int port = 1337;
+        try (ServerSocket ss = new ServerSocket(port)) {
+            System.out.println("Listening on :" + port);
+            while (true) {
+                Socket socket = ss.accept();
+                RequestHandler handler = new RequestHandler(socket);
+                new Thread(handler).start();
+            }
+        }
     }
 }
